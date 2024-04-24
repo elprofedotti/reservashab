fa0fcb47fe6c1f425e358a3a519bde8fa5eac592
elprofedotti <elprofedotti@gmail.com>
2024-04-23 21:18:28 -0300

    asdf

--- includes/config/database.php
+++ includes/config/database.php
@@ -3,7 +3,7 @@
 
 function conectarDB() : mysqli{
     
-    $db=new mysqli('89.116.115.122', 'u414612939_breprofe', 'H3lito$2024','u414612939_bienesraices_');
+    $db=new mysqli('localhost', 'root', 'pass','bd_crud');
 
     if(!$db){
         echo "ERROR no se conecto";
